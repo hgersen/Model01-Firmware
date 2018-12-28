520b881d43add570a50cf0b7156ebd20d65935f8
Henkjan Gersen <h.gersen@gmail.com>
2018-12-28 20:54:49 +0000

    strip out mousekeys

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -3,7 +3,7 @@
 // See "LICENSE" for license details
 
 #ifndef BUILD_INFORMATION
-#define BUILD_INFORMATION "locally built"
+#define BUILD_INFORMATION "built by hgersen"
 #endif
 
 /**
@@ -20,9 +20,6 @@
 
 // Support for communicating with the host via a simple Serial protocol
 #include "Kaleidoscope-FocusSerial.h"
-
-// Support for keys that move the mouse
-#include "Kaleidoscope-MouseKeys.h"
 
 // Support for macros
 #include "Kaleidoscope-Macros.h"
@@ -219,9 +216,6 @@ KALEIDOSCOPE_INIT_PLUGINS(
 
   // The macros plugin adds support for macros
   Macros,
-
-  // The MouseKeys plugin lets you add keys to your keymap which move the mouse.
-  MouseKeys,
 
   // The HostPowerManagement plugin allows us to turn LEDs off when then host
   // goes to sleep, and resume them when it wakes up.
